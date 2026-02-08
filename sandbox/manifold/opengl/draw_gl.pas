85b8c05acb2c5344b2480cb2bc402cb43490a0de
sechshelme <m_burkhard@gmx.ch>
2026-02-08 20:12:25 +0100

    manifold

--- sandbox/manifold/opengl/draw_gl.pas
+++ sandbox/manifold/opengl/draw_gl.pas
@@ -24,22 +24,27 @@ function malloc(__size: Tsize_t): pointer; cdecl; external libc;
 procedure free(__ptr: pointer); cdecl; external libc;
 
 
+//  OpenSCAD
+
 implementation
 
 type
   TVector3f = array[0..2] of single;
+  Tbuf = array[0..15] of byte;
 
 procedure draw;
 var
   m_size: Tsize_t;
   cube_mem, holeX_mem, holeY_mem, holeZ_mem, diff1_mem, diff2_mem,
-  final_mem, mesh_mem: Pointer;
-  cube, holeX, holeY, holeZ, res1, res2, final_res: PManifoldManifold;
+  final_mem: Tbuf;
+  mesh_mem, normal_mem: Pointer;
+  cube, holeX, holeY, holeZ, res1, res2, final_res, final_w_normals: PManifoldManifold;
+
   mesh: PManifoldMeshGL;
   n_verts, n_tris, n_props: longint;
-  verts_data: PSingle;
-  tris_data: Puint32_t;
-  x, y, z: single;
+  verts_data: array of single;
+  tris_data: array of Tuint32_t;
+  x, y, z, nx, ny, nz: single;
   i: integer;
   a, b, c: Tuint32_t;
 
@@ -48,43 +53,49 @@ var
 
 begin
   m_size := manifold_manifold_size();
-  WriteLn(m_size);
 
-  cube_mem := malloc(m_size);
-  holeX_mem := malloc(m_size);
-  holeY_mem := malloc(m_size);
-  holeZ_mem := malloc(m_size);
-  diff1_mem := malloc(m_size);
-  diff2_mem := malloc(m_size);
-  final_mem := malloc(m_size);
-  mesh_mem := malloc(manifold_meshgl_size);
+  mesh_mem := GetMem(manifold_meshgl_size);
+  //  WriteLn(manifold_meshgl_size);
 
-  cube := manifold_cube(cube_mem, 10.0, 10.0, 10.0, 1);
+  cube := manifold_cube(@cube_mem, 10.0, 10.0, 10.0, 1);
 
-  holeX := manifold_cube(holeX_mem, 20.0, 4.0, 4.0, 1);
-  holeY := manifold_cube(holeY_mem, 4.0, 20.0, 4.0, 1);
-  holeZ := manifold_cube(holeZ_mem, 4.0, 4.0, 20.0, 1);
+  holeX := manifold_cube(@holeX_mem, 20.0, 4.0, 4.0, 1);
 
-  res1 := manifold_difference(diff1_mem, cube, holeX);
-  res2 := manifold_difference(diff2_mem, res1, holeY);
-  final_res := manifold_difference(final_mem, res2, holeZ);
+  holeY := manifold_cube(@holeY_mem, 4.0, 20.0, 4.0, 1);
+  //  holeZ := manifold_cube(@holeZ_mem, 4.0, 4.0, 20.0, 1);
+
+  holeZ := manifold_cylinder(@holeZ_mem, 20.0, 2.0, 2.0, 32, 1);
+
+
+  res1 := manifold_difference(@diff1_mem, cube, holeX);
+  res2 := manifold_difference(@diff2_mem, res1, holeY);
+  final_res := manifold_difference(@final_mem, res2, holeZ);
 
   // --- 4. DATEN-EXTRAKTION ---
-  mesh := manifold_get_meshgl(mesh_mem, final_res);
+
+
+  normal_mem := malloc(manifold_manifold_size());
+  final_w_normals := manifold_calculate_normals(normal_mem, final_res, 3, 60.0);
+
+
+  mesh := manifold_get_meshgl(mesh_mem, final_w_normals);
+
 
   n_verts := manifold_meshgl_num_vert(mesh);
   n_tris := manifold_meshgl_num_tri(mesh);
   n_props := manifold_meshgl_num_prop(mesh);
 
-  verts_data := PSingle(malloc(n_verts * n_props * sizeof(single)));
-  tris_data := Puint32_t(malloc(n_tris * 3 * sizeof(Tuint32_t)));
+  SetLength(verts_data, n_verts * n_props);
+  SetLength(tris_data, n_tris * 3);
 
-  manifold_meshgl_vert_properties(verts_data, mesh);
-  manifold_meshgl_tri_verts(tris_data, mesh);
+  manifold_meshgl_vert_properties(PSingle(verts_data), mesh);
+  manifold_meshgl_tri_verts(Puint32_t(tris_data), mesh);
+
 
   WriteLn('--- MANIFOLD CSG ERGEBNIS ---');
-  WriteLn('Objekt: Würfel mit 3 Achsen-Bohrungen (Menger Stufe 1)');
-  WriteLn('Stats: %zu Vertices, %zu Dreiecke', n_verts, '  ', n_tris);
+  WriteLn('n_verts: ', n_verts);
+  WriteLn('n_tris: ', n_tris);
+  WriteLn('n_props: ', n_props);
 
 
   WriteLn(#10'Liste aller Eckpunkte (Vertices):');
@@ -95,7 +106,14 @@ begin
     x := verts_data[i * n_props + 0];
     y := verts_data[i * n_props + 1];
     z := verts_data[i * n_props + 2];
-    WriteLn(i: 3, '  ', x: 4: 2, '/', y: 4: 2, '/', z: 4: 2);
+
+    nx := verts_data[i * n_props + 6];
+    ny := verts_data[i * n_props + 7];
+    nz := verts_data[i * n_props + 8];
+
+
+
+    WriteLn(i: 3, '  ', x: 4: 2, '/', y: 4: 2, '/', z: 4: 2, ' - ', nx: 4: 2, '/', ny: 4: 2, '/', nz: 4: 2);
 
     vectors[i][0] := x;
     vectors[i][1] := y;
@@ -127,26 +145,16 @@ begin
 
   end;
 
-  manifold_delete_manifold(cube);
-  manifold_delete_manifold(holeX);
-  manifold_delete_manifold(holeY);
-  manifold_delete_manifold(holeZ);
-  manifold_delete_manifold(res1);
-  manifold_delete_manifold(res2);
-  manifold_delete_manifold(final_res);
-  manifold_delete_meshgl(mesh);
+  manifold_destruct_manifold(cube);
+  manifold_destruct_manifold(holeX);
+  manifold_destruct_manifold(holeY);
+  manifold_destruct_manifold(holeZ);
+  manifold_destruct_manifold(res1);
+  manifold_destruct_manifold(res2);
+  manifold_destruct_manifold(final_res);
+  manifold_destruct_meshgl(mesh);
 
-  // --- 6. CLEANUP ---
-  free(verts_data);
-  free(tris_data);
-  //free(cube_mem);
-  //free(holeX_mem);
-  //free(holeY_mem);
-  //free(holeZ_mem);
-  //free(diff1_mem);
-  //free(diff2_mem);
-  //free(final_mem);
-  //free(mesh_mem);
+  Freemem(mesh_mem);
 end;
 
 end.
