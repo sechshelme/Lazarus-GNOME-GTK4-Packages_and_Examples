3b10bb72a1fba0a2d5155c5faa4f7e2c8800a550
sechshelme <m_burkhard@gmx.ch>
2026-02-28 17:39:43 +0100

    lab

--- sandbox/las_LCL/unit1.pas
+++ sandbox/las_LCL/unit1.pas
@@ -13,6 +13,7 @@ type
   { TForm1 }
 
   TForm1 = class(TForm)
+    procedure FormCreate(Sender: TObject);
     procedure FormPaint(Sender: TObject);
   private
 
@@ -29,59 +30,107 @@ implementation
 
 { TForm1 }
 
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  Color := clBlack;
+end;
+
 procedure TForm1.FormPaint(Sender: TObject);
+// LiDAR-Daten
 const
-  filename = '/home/tux/Schreibtisch/von_Git/liblas/libLAS/test/data/autzen.las';
+  filename = '/home/tux/Schreibtisch/von_Git/liblas/libLAS/test/data/TO_core_last_zoom.las';
+  //  filename = '/home/tux/Schreibtisch/2711_1245.las';
 var
   reader: TLASReaderH;
   header: TLASHeaderH;
   points_count: DWord;
-  version_major, version_minor: byte;
+  version_major, version_minor, cl: byte;
   p: TLASPointH;
-  i: integer;
-  x, y: Int64;
+  i, winW, winH, winZ: integer;
+  px, py, pz: int64;
+  minX, maxX, minY, maxY, rangeX, rangeY, minZ, maxZ, rangeZ: double;
+  col: TColor;
 begin
-      reader := LASReader_Create(filename);
-    if reader = nil then begin
-      WriteLn('Fehler beim Öffnen!');
-      Exit;
+  reader := LASReader_Create(filename);
+  if reader = nil then begin
+    WriteLn('Fehler beim Öffnen!');
+    Exit;
+  end;
+
+  header := LASReader_GetHeader(reader);
+
+  points_count := LASHeader_GetPointRecordsCount(header);
+  version_major := LASHeader_GetVersionMajor(header);
+  version_minor := LASHeader_GetVersionMinor(header);
+
+  WriteLn('Datei: ', filename);
+  WriteLn('LAS Version:  ', version_major, '.', version_minor);
+  WriteLn('Anzahl Punkte: ', points_count);
+
+  WriteLn('Lese Punkte...');
+
+  winW := ClientWidth;
+  winH := ClientHeight;
+  winZ := 255;
+
+  minX := LASHeader_GetMinX(header);
+  maxX := LASHeader_GetMaxX(header);
+  minY := LASHeader_GetMinY(header);
+  maxY := LASHeader_GetMaxY(header);
+  minZ := LASHeader_GetMinZ(header);
+  maxZ := LASHeader_GetMaxZ(header);
+
+  rangeX := maxX - minX;
+  rangeY := maxY - minY;
+  rangeZ := maxZ - minZ;
+
+  for i := 0 to points_count - 1 do begin
+    p := LASReader_GetNextPoint(reader);
+    if p = nil then begin
+      break;
     end;
 
-    header := LASReader_GetHeader(reader);
+    if rangeX > 0 then begin
+      px := Trunc(((LASPoint_GetX(p) - minX) / rangeX) * winW);
+    end;
 
-    points_count := LASHeader_GetPointRecordsCount(header);
-    version_major := LASHeader_GetVersionMajor(header);
-    version_minor := LASHeader_GetVersionMinor(header);
+    if rangeY > 0 then begin
+      py := winH - Trunc(((LASPoint_GetY(p) - minY) / rangeY) * winH);
+    end;
 
-    WriteLn('Datei: ', filename);
-    WriteLn('LAS Version:  ', version_major, '.', version_minor);
-    WriteLn('Anzahl Punkte: ', points_count);
+    if rangeZ > 0 then begin
+      pz := Trunc(((LASPoint_GetZ(p) - minZ) / rangeZ) * winZ);
+    end;
 
-    LASHeader_Destroy(header);
+    cl := LASPoint_GetClassification(p);
 
-    WriteLn('Lese Punkte...');
-
-    for i := 0 to points_count - 1 do begin
-      p := LASReader_GetNextPoint(reader);
-      if p = nil then begin
-        break;
+    WriteLn(px, ' x ', py, '   ', pz);
+    case cl of
+      1: begin
+        col := pz;
       end;
-//      x:=Trunc( LASPoint_GetX(p)/10000);
-//      y:=Trunc( LASPoint_GetY(p)/10000);
-      x:=Trunc( LASPoint_GetX(p)-637000);
-      y:=Trunc( LASPoint_GetY(p)-852000);
-      WriteLn(x,' x ',y);
-
-      Canvas.Pixels[x,y]:=0;
-
-      WriteLn('P ', i: 2, ': X=', LASPoint_GetX(p): 0: 2, ' Y=', LASPoint_GetY(p): 0: 2, ' Z=', LASPoint_GetZ(p): 0: 2,
-        ' | Int: ', LASPoint_GetIntensity(p), ' | Class: ', LASPoint_GetClassification(p));
+      2: begin
+        col := pz * $100;
+      end;
+      else begin
+        col := pz * $10000;
+      end;
     end;
+    col := pz;
+    //    Canvas.Pen.Color:=col;
+    //    Canvas.Brush.Color:=col;
+    //    Canvas.FillRect(px-2, py-2, px+2, py+2);
+    Canvas.Pixels[px, py] := col;
 
-    LASReader_Destroy(reader);
+    WriteLn('P ', i: 2, ': X=', LASPoint_GetX(p): 0: 2, ' Y=', LASPoint_GetY(p): 0: 2, ' Z=', LASPoint_GetZ(p): 0: 2,
+      ' | Int: ', LASPoint_GetIntensity(p), ' | Class: ', LASPoint_GetClassification(p));
+  end;
 
+  LASHeader_Destroy(header);
+
+
+  LASReader_Destroy(reader);
 
 end;
 
 end.
-
