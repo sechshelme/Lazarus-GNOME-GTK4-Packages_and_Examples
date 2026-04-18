ee441f8fd98b02fbf1b4d5596e1d49062df48cb6
sechshelme <m_burkhard@gmx.ch>
2026-04-18 16:08:59 +0200

    beltdrive

--- Example/GTK4_and_Extensions/GTK4/32_-_Cairo_Belt_Drive/mywidget.pas
+++ Example/GTK4_and_Extensions/GTK4/32_-_Cairo_Belt_Drive/mywidget.pas
@@ -3,24 +3,24 @@ unit MyWidget;
 interface
 
 uses
-  fp_glib2, fp_GTK4;
+  Math,
+  fp_glib2, fp_GTK4, fp_cairo, fp_graphene;
 
 type
   TMyWidget = record
-    parent_instance: TGtkDrawingArea;
-    x1, y1, x2, y2: double;
+    parent_instance: TGtkWidget;
+    r1, r2, x1, y1, x2, y2: double;
   end;
   PMyWidget = ^TMyWidget;
 
   TMyWidgetClass = record
-    parent_class: TGtkDrawingAreaClass;
+    parent_class: TGtkWidgetClass;
   end;
   PMyWidgetClass = ^TMyWidgetClass;
 
 function my_widget_get_type: TGType;
 function my_widget_new: PGTKWidget;
-procedure my_widget_set_coords(self: PMyWidget; x1, y1, x2, y2: double);
-procedure my_widget_get_coords(self: PMyWidget; x1, y1, x2, y2: Pdouble);
+procedure my_widget_set_coords(self: PMyWidget; r1, x1, y1, r2, x2, y2: double);
 
 implementation
 
@@ -31,11 +31,77 @@ var
   self: PMyWidget absolute instance;
 begin
   with self^ do begin
-    x1 := 0.0;
-    y1 := 0.0;
-    x2 := 0.0;
-    y2 := 0.0;
+    r1 := 10.0;
+    r2 := 10.0;
+    x1 := 10.0;
+    y1 := 10.0;
+    x2 := 100.0;
+    y2 := 100.0;
   end;
+end;
+
+procedure snapshot_cp(widget: PGtkWidget; snapshot: PGtkSnapshot); cdecl;
+var
+  self: PMyWidget absolute widget;
+  cr: Pcairo_t;
+  r: Tgraphene_rect_t;
+  width, height: longint;
+
+  dx, dy, dist, angle, alpha: double;
+begin
+  width := gtk_widget_get_width(widget);
+  height := gtk_widget_get_height(widget);
+
+  graphene_rect_init(@r, 0, 0, width, height);
+  gtk_snapshot_push_clip(snapshot, @r);
+
+  graphene_rect_init(@r, 0.0, 0.0, width, height);
+  cr := gtk_snapshot_append_cairo(snapshot, @r);
+
+  with self^ do begin
+    dx := x2 - x1;
+    dy := y2 - y1;
+    dist := Sqrt(dx * dx + dy * dy);
+    angle := ArcTan2(dy, dx);
+    alpha := ArcCos((r1 - r2) / dist);
+
+    cairo_set_source_rgb(cr, 0.3, 0.3, 0.3);
+    cairo_paint(cr);
+
+    // Kreise zeichnen
+    cairo_set_source_rgb(cr, 1.0, 0.5, 0.5);
+    cairo_arc(cr, x1, y1, r1, 0, 2 * Pi);
+    cairo_fill(cr);
+    cairo_set_source_rgb(cr, 0.5, 0.5, 1.0);
+    cairo_arc(cr, x2, y2, r2, 0, 2 * Pi);
+    cairo_fill(cr);
+
+    // Riemen zeichnen
+    cairo_set_source_rgb(cr, 0.7, 0.7, 0.2);
+    cairo_set_line_width(cr, 5);
+
+    cairo_arc(cr, x1, y1, r1, angle + alpha, angle - alpha);
+    cairo_stroke(cr);
+    cairo_arc(cr, x2, y2, r2, angle - alpha, angle + alpha);
+    cairo_stroke(cr);
+
+    cairo_move_to(cr, x1 + r1 * Cos(angle + alpha), y1 + r1 * Sin(angle + alpha));
+    cairo_line_to(cr, x2 + r2 * Cos(angle + alpha), y2 + r2 * Sin(angle + alpha));
+    cairo_stroke(cr);
+
+    cairo_move_to(cr, x1 + r1 * Cos(angle - alpha), y1 + r1 * Sin(angle - alpha));
+    cairo_line_to(cr, x2 + r2 * Cos(angle - alpha), y2 + r2 * Sin(angle - alpha));
+    cairo_stroke(cr);
+  end;
+
+  cairo_destroy(cr);
+
+  gtk_snapshot_pop(snapshot);
+end;
+
+procedure init_class_cp(g_class: Tgpointer; class_data: Tgpointer); cdecl;
+begin
+  GTK_WIDGET_CLASS(g_class)^.snapshot := @snapshot_cp;
 end;
 
 
@@ -48,33 +114,26 @@ var
   id: TGType;
 begin
   if g_once_init_enter(@type_id) then begin
-    id := g_type_register_static_simple(GTK_TYPE_DRAWING_AREA, 'MyWidget', SizeOf(TMyWidgetClass), nil, SizeOf(TMyWidget), @init_cp, 0);
+    id := g_type_register_static_simple(GTK_TYPE_WIDGET, 'MyWidget', SizeOf(TMyWidgetClass), @init_class_cp, SizeOf(TMyWidget), @init_cp, 0);
     g_once_init_leave(@type_id, id);
   end;
   Result := type_id;
 end;
 
 function my_widget_new: PGTKWidget;
-var
-  self: PMyWidget absolute Result;
 begin
   Result := g_object_new(my_widget_get_type, nil);
 end;
 
-procedure my_widget_set_coords(self: PMyWidget; x1, y1, x2, y2: double);
+procedure my_widget_set_coords(self: PMyWidget; r1, x1, y1, r2, x2, y2: double);
 begin
+  self^.r1 := r1;
   self^.x1 := x1;
   self^.y1 := y1;
+  self^.r2 := r2;
   self^.x2 := x2;
   self^.y2 := y2;
-end;
-
-procedure my_widget_get_coords(self: PMyWidget; x1, y1, x2, y2: Pdouble);
-begin
-  x1^ := self^.x1;
-  y1^ := self^.y1;
-  x2^ := self^.x2;
-  y2^ := self^.y2;
+  gtk_widget_queue_draw(GTK_WIDGET(self));
 end;
 
 end.
--- Example/GTK4_and_Extensions/GTK4/32_-_Cairo_Belt_Drive/project1.lpr
+++ Example/GTK4_and_Extensions/GTK4/32_-_Cairo_Belt_Drive/project1.lpr
@@ -9,7 +9,7 @@ uses
 
 type
   TAppData = record
-    //    x1, y1, x2, y2: double;
+    //       r1, r2, x1, y1, x2, y2: double;
   end;
   PAppData = ^TAppData;
 
@@ -20,70 +20,32 @@ type
     gtk_window_destroy(window);
   end;
 
-  procedure draw_func(drawing_area: PGtkDrawingArea; cr: Pcairo_t; Width: longint; Height: longint; user_data: Tgpointer); cdecl;
-  var
-    x1, y1, r1: double;
-    x2, y2, r2: double;
-    dx, dy, dist, angle, alpha: double;
-  var
-    self: PMyWidget absolute drawing_area;
-  begin
-    x1 := self^.x1 + 150;
-    y1 := self^.y1 + 180;
-    r1 := 100;
-    x2 := self^.x2 + 550;
-    y2 := self^.y2 + 180;
-    r2 := 15;
-    dx := x2 - x1;
-    dy := y2 - y1;
-    dist := Sqrt(dx * dx + dy * dy);
-    angle := ArcTan2(dy, dx);
-    alpha := ArcCos((r1 - r2) / dist);
-
-    cairo_set_source_rgb(cr, 0.3, 0.3, 0.3);
-    cairo_paint(cr);
-
-    // Kreise zeichnen
-    cairo_set_source_rgb(cr, 1.0, 0.5, 0.5);
-    cairo_arc(cr, x1, y1, r1, 0, 2 * Pi);
-    cairo_fill(cr);
-    cairo_set_source_rgb(cr, 0.5, 0.5, 1.0);
-    cairo_arc(cr, x2, y2, r2, 0, 2 * Pi);
-    cairo_fill(cr);
-
-    // Riemen zeichnen
-    cairo_set_source_rgb(cr, 0.7, 0.7, 0.2);
-    cairo_set_line_width(cr, 5);
-
-    cairo_arc(cr, x1, y1, r1, angle + alpha, angle - alpha);
-    cairo_stroke(cr);
-    cairo_arc(cr, x2, y2, r2, angle - alpha, angle + alpha);
-    cairo_stroke(cr);
-
-    cairo_move_to(cr, x1 + r1 * Cos(angle + alpha), y1 + r1 * Sin(angle + alpha));
-    cairo_line_to(cr, x2 + r2 * Cos(angle + alpha), y2 + r2 * Sin(angle + alpha));
-    cairo_stroke(cr);
-
-    cairo_move_to(cr, x1 + r1 * Cos(angle - alpha), y1 + r1 * Sin(angle - alpha));
-    cairo_line_to(cr, x2 + r2 * Cos(angle - alpha), y2 + r2 * Sin(angle - alpha));
-    cairo_stroke(cr);
-  end;
-
   function on_tick(widget: PGtkWidget; frame_clock: PGdkFrameClock; user_data: Tgpointer): Tgboolean; cdecl;
   var
     self: PMyWidget absolute widget;
-    x1, y1, x2, y2, current_time: double;
+    r1, r2, y1, y2, current_time: double;
+    width, height: longint;
+
+    ofs_x1: single;
+    ofs_y1: single;
+    ofs_x2: single;
+    ofs_y2: single;
+
   begin
+    width := gtk_widget_get_width(widget);
+    height := gtk_widget_get_height(widget);
+
+    ofs_x1 := 150;
+    ofs_y1 := 180;
+    ofs_x2 := width - 100;
+    ofs_y2 := height - 150;
+
     current_time := gdk_frame_clock_get_frame_time(frame_clock) / 500000.0;
-
-    my_widget_get_coords(self, @x1, @y1, @x2, @y2);
-
     y1 := sin(current_time) * 50.0;
     y2 := sin(current_time * 1.13) * 80.0;
-
-    my_widget_set_coords(self, x1, y1, x2, y2);
-
-    gtk_widget_queue_draw(widget);
+    r1 := 80.0 + sin(current_time) * 20.0;
+    r2 := 20.0 + sin(current_time) * 5.0;
+    my_widget_set_coords(self, r1, ofs_x1, ofs_y1 + y1, r2, ofs_x2, ofs_y2 + y2);
 
     Result := G_SOURCE_CONTINUE;
   end;
@@ -120,11 +82,8 @@ type
     drawing_area := my_widget_new;
     gtk_widget_set_vexpand(drawing_area, True);
     gtk_widget_set_hexpand(drawing_area, True);
-    gtk_drawing_area_set_draw_func(GTK_DRAWING_AREA(drawing_area), @draw_func, nil, nil);
     gtk_widget_add_tick_callback(drawing_area, @on_tick, nil, nil);
     gtk_box_append(GTK_BOX(box), drawing_area);
-
-    my_widget_set_coords(PMyWidget(drawing_area), 0.0, 0.0, 0.0, 0.0);
 
     button := gtk_button_new_with_label('Quit');
     g_signal_connect(button, 'clicked', G_CALLBACK(@quit_cp), window);
