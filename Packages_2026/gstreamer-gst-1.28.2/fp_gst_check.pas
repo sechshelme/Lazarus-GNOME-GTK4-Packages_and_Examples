82b99395513682666d457c0b1108b29a7d7d731c
sechshelme <m_burkhard@gmx.ch>
2026-07-25 19:49:47 +0200

    gst_check

--- Packages_2026/gstreamer-gst-1.28.2/fp_gst_check.pas
+++ Packages_2026/gstreamer-gst-1.28.2/fp_gst_check.pas
@@ -4,34 +4,35 @@ interface
 
 uses
   fp_glib2,
-  fp_gst,
-  fp_gst_base,
-  Strings,
-  ctypes;
+  fp_gst;
 
   {$IFDEF FPC}
   {$PACKRECORDS C}
   {$ENDIF}
 
+type
+  PSuite = type Pointer;
+  PTCase = type Pointer;
+  PSRunner = type Pointer;
+
   {$DEFINE read_interface}
-  //{$include gst/check/internal_check.inc}                      // io.                      // viele Makros entfernt   lib CK_DLL_EXP  ?????
-  //{$include gst/check/gstcheck.inc}                            // io. -> internal_check    // viele Makros entfernt
-  //{$include gst/check/gstbufferstraw.inc}                      // io.
-  //{$include gst/check/gstconsistencychecker.inc}               // io.
-  //{$include gst/check/gsttestclock.inc}                        // io.
-  //{$include gst/check/gstharness.inc}                          // io. -> gsttestclock
+  {$include gst/check/gstcheck.inc}
+  {$include gst/check/gstbufferstraw.inc}
+  {$include gst/check/gstconsistencychecker.inc}
+  {$include gst/check/gsttestclock.inc}
+  {$include gst/check/gstharness.inc}
+  {$include gst/check/internal_check.inc}
   {$UNDEF read_interface}
 
 implementation
 
 {$DEFINE read_implementation}
-//{$include gst/check/internal_check.inc}                      // io.                      // viele Makros entfernt   lib CK_DLL_EXP  ?????
-//{$include gst/check/gstcheck.inc}                            // io. -> internal_check    // viele Makros entfernt
-//{$include gst/check/gstbufferstraw.inc}                      // io.
-//{$include gst/check/gstconsistencychecker.inc}               // io.
-//{$include gst/check/gsttestclock.inc}                        // io.
-//{$include gst/check/gstharness.inc}                          // io. -> gsttestclock
-//{$UNDEF read_interface}
+{$include gst/check/gstcheck.inc}
+{$include gst/check/gstbufferstraw.inc}
+{$include gst/check/gstconsistencychecker.inc}
+{$include gst/check/gsttestclock.inc}
+{$include gst/check/gstharness.inc}
+{$include gst/check/internal_check.inc}
 {$UNDEF read_implementation}
 
 end.
